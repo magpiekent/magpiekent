b0509e366107556e44a82c3080b758f72092baab
Magpie Kent <magpiekent@gmail.com>
2017-12-13 14:30:58 -0500

    Update cycle.js

--- cycle.js
+++ cycle.js
@@ -1,8 +1,8 @@
 var hi = new Array ();
 hi[0] = "<img class="img-fluid mx-auto d-block" src="main-magpie.png"/>";
 hi[1] = "<img class="img-fluid mx-auto d-block" src="butt.png"/>";
-hi[2] = "<img class="img-fluid mx-auto d-block" src="team-3.png"/>";
-hi[3] = "<img class="img-fluid mx-auto d-block" src="team-2.png"/>";
+hi[2] = "<img class="img-fluid mx-auto d-block" src="cattongue.jpg"/>";
+hi[3] = "<img class="img-fluid mx-auto d-block" src="cattongue.jpg"/>";
 var i = Math.floor(4*Math.random())
 //this number ^ must always be one more than however many greetings there are!!!
 document.write(hi[i]);
